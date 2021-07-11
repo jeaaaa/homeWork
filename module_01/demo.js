3dbd32c972899fa0c4a3c5fc6ac24d57d928eec1
jeaaaa <835755354@qq.com>
2021-07-11 23:41:00 +0800

    64

--- module_01/demo.js
+++ module_01/demo.js
@@ -1,23 +1,24 @@
-const MyNumberConvertion = require('./my-number-convertion');
+const Conversion = require('./incrementalConversion');
 
-console.log('64进制');
+// console.log('64进制');
 // //初始化的字符串，字符串长度就是进制，从左到右，0对应数值0，最后一个字符~对应数值63
-const chars64 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-~';
+const chars64 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-';
 
 // //初始化转换器
-const convertion64 = new MyNumberConvertion(chars64);
-let str64 = '445306828';
-let num64 = convertion64.transfer(str64);//将数字转换成64进制
-let source64 = convertion64.revert(num64);//将64进制字符还原成原始数据
-console.log(`原始数据：${str64}`);
+const convertion64 = new Conversion(chars64);
+let str64 = 1.1
+let num64 = convertion64.encode(str64);//将数字转换成64进制
+// let source64 = convertion64.revert(num64);//将64进制字符还原成原始数据
+// console.log(`原始数据：${str64}`);
 console.log(`转换后：${num64}`)
-console.log(`还原后：${source64}`);
+// console.log(`还原后：${source64}`);
 
 // console.log('2进制');
 // const convertion2 = new MyNumberConvertion('01');
 // let str2 = 15;
 // let num2 = convertion2.transfer(str2);
 // let source2 = convertion2.revert(num2);
+
 // console.log(`原始数据：${str2}`);
 // console.log(`转换后：${num2}`)
 // console.log(`还原后：${source2}`);
--- module_01/incrementalConversion.js
+++ module_01/incrementalConversion.js
@@ -1,3 +1,4 @@
+
 /**
  * 十进制数转换成其他进制
  * 整数--"除二取余，逆序排列"
@@ -7,14 +8,100 @@
 // Math.trunc 取整
 // 9.67 % 1  取小数
 
-class conversion {
+/**
+ * @description: 
+ * @param {*}
+ * @return {*}
+ */
+class Conversion {
     constructor(char) {
+        this.char = char
+        this.STEP = char.length
+        this.map = new Map()
+        for (let i = 0; i < this.STEP; i++) {
+            let c = char[i]
+            this.map.set(c, i)
+        }
+    }
+    /**
+     * @description: 
+     * @param {*} num
+     * @return {*}
+     */
+    encode(num) {
+        if (isNaN(num)) throw new Error('请传入number~')
+        if (num > Number.MAX_SAFE_INTEGER || num < Number.MIN_SAFE_INTEGER) {
+            throw new Error('超出安全数范围~')
+        }
+
+        let neg = false
+        // 记录符号
+        if (num < 0) {
+            neg = true
+            num = -1 * num
+        }
+
+        let numArr = num.toString().split('.')  // 拆分整数和小数，避免精度误差
+        console.log(numArr)
+        let int = this.encodeInt(numArr[0])
+        let float = numArr[1] ? this.encodeFloat(numArr[1]) : ''
+        let floatStr = float ? `.${float}` : ''
+
+        return neg ? `-${int}${floatStr}` : `${int}${floatStr}`
 
     }
-    encode() {
+    incode(str) {
 
     }
-    incode() {
+    /**
+     * @description: 转换整数
+     * @param {*} int
+     * @return {*}  对应的整数进制部分
+     */
+    encodeInt(int) {
+        // 除相应进制取余，到0结束，逆序排列
+        let a = +int   //把字符串变成数字
+        let intArr = []
+        if (a === 0) {
+            intArr.push('0')
+        }
+        const STEP = this.STEP
+        while (a > 0) {
+            let tmp = a % STEP
+            let c = this.char[tmp]
+            intArr.unshift(c)
+            a = parseInt(a / STEP)
+        }
+        return intArr.join('')
+    }
+    /**
+     * @description: 转换小数
+     * @param {*} float
+     * @return {*}  对应的小数进制部分
+     */
+    encodeFloat(float) {
+        // 乘相应进制取整，到1结束，顺序排列
+        let b = +`0.${float}`
+        console.log(b)
+        let floatArr = []
+        if (b === 1) {
+            floatArr.push('1')
+        }
+        const STEP = this.STEP
+        while (b < 1) {
+            let tmp = Math.trunc(b * STEP)
+            let c = this.char[tmp]
+            floatArr.push(c)
+            b = (b * STEP).toString().split('.')[1]
+        }
+        return floatArr.join('')
+    }
+    incodeInt() {
+
+    }
+    incodeFloat() {
 
     }
 }
+
+module.exports = Conversion;
