93fd2abf44f0c688c5adef99da8f266c776024a0
jeaaaa <835755354@qq.com>
2021-07-12 23:30:00 +0800

    pic

--- module_01/pic/src/App.js
+++ module_01/pic/src/App.js
@@ -1,11 +1,15 @@
 // 读取本地图片转换成灰度图保存。
 import './App.css';
+import React from 'react';
+import { Button } from 'antd';
+
 
 function App() {
 
     return (
         <div className="App">
-
+            <input type="file" accept="" style="display: none;"></input>
+            <Button type="primary">Button</Button>
         </div>
     );
 }
