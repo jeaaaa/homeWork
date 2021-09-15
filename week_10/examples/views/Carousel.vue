14315430d4e72d6032baf7766a1c2826af64291d
jeaaaa <835755354@qq.com>
2021-09-15 21:30:31 +0800

    work

--- week_10/examples/views/Carousel.vue
+++ week_10/examples/views/Carousel.vue
@@ -2,24 +2,43 @@
 <template>
     <div class="page-carousel">
         <h2>Carousel 走马灯</h2>
-        <Carousel></Carousel>
+        <Carousel height="150px">
+            <Carousel-item v-for="item in 4" :key="item">
+                <h3 class="small">{{ item }}</h3>
+            </Carousel-item>
+        </Carousel>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, toRefs, computed, h } from "vue";
-import type { PropType } from 'vue';
-import { Carousel } from "@/ele";
+import { defineComponent, ref, toRefs, computed, h } from 'vue'
+import type { PropType } from 'vue'
+import { Carousel } from '@/ele'
 
 export default defineComponent({
     name: 'PageCarousel',
     components: { Carousel },
+
     setup(props, { emit, slots }) {
-        return {
-        }
+        return {}
     }
-});
+})
 </script>
 
-<style lang="scss" scoped>
+<style>
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+}
 </style>
--- week_10/examples/views/Radio.vue
+++ week_10/examples/views/Radio.vue
@@ -2,23 +2,29 @@
 <template>
     <div class="page-radio">
         <h2>Radio 单选框</h2>
-        <Radio></Radio>
+        <Radio v-model="radio1" label="1">备选项1</Radio>
+        <Radio v-model="radio1" label="2">备选项2</Radio>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, toRefs, computed, h } from "vue";
-import type { PropType } from 'vue';
-import { Radio } from "@/ele";
+import { defineComponent, ref, toRefs, computed, h } from 'vue'
+import type { PropType } from 'vue'
+import { Radio } from '@/ele'
 
 export default defineComponent({
     name: 'PageRadio',
     components: { Radio },
-    setup(props, { emit, slots }) {
+    data() {
         return {
+            radio1: '1',
+            radio2: '1'
         }
+    },
+    setup(props, { emit, slots }) {
+        return {}
     }
-});
+})
 </script>
 
 <style lang="scss" scoped>
