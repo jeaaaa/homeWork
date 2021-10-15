e710ed890afd7806b26f08d437614b682df1cf4e
180319 <835755254@tslsmart.com>
2021-10-15 16:37:11 +0800

    work

---
+++ week_13/ostrich-server/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: jea
+ * @Date: 2021-10-15 16:36:48
+ * @LastEditTime: 2021-10-15 16:36:50
+ * @LastEditors: jea
+ * @Description:
+ * @FilePath: \homeWork\week_13\ostrich-server\index.ts
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+interface io {
+
+}
