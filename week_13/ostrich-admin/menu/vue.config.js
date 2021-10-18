2c727cf7dc43c72a252bee983b34848926931d99
180319 <835755254@tslsmart.com>
2021-10-18 17:40:59 +0800

    work

--- week_13/ostrich-admin/menu/vue.config.js
+++ week_13/ostrich-admin/menu/vue.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  devServer: {
-    disableHostCheck: true,
-    host: "0.0.0.0",
-    port: 8080,
-    // allowedHosts: "all",
-    https: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
-      "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+    devServer: {
+        disableHostCheck: true,
+        host: "0.0.0.0",
+        port: 8080,
+        // allowedHosts: "all",
+        https: false,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        },
+        proxy: {
+            "^/project": {
+                target: "http://localhost:5000/",
+                changeOrigin: true,
+                // pathRewrite: {
+                //   "/device": "",
+                // },
+            },
+        },
     },
-    proxy: {
-      "^/project": {
-        target: "http://localhost:5000/",
-        changeOrigin: true,
-        // pathRewrite: {
-        //   "/device": "",
-        // },
-      },
+    transpileDependencies: ["common"],
+    chainWebpack: (config) => {
+        config.plugin("define").tap((args) => {
+            console.log(args[0]["process.env"]);
+            return args;
+        });
     },
-  },
-  transpileDependencies: ["common"],
-  chainWebpack: (config) => {
-    config.plugin("define").tap((args) => {
-      console.log(args[0]["process.env"]);
-      return args;
-    });
-  },
 };
